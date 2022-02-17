0f639fdcbb7fb7d66b8d75c84ca05f3a11f171b5
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2022-02-16 18:07:24 -0800

    Request to upload 2.16.22

---
+++ HQRipperv1.2.0.py
@@ -0,0 +1,42 @@
+### this is a gilvasunner certified haltmann works program it generates a mp3 file from a selected youtube link and remixes it with a selected mp3 file from 2 or more youtube urls
+## this is the default version of the program
+## (C) Grand Dad Enterprises
+
+def __init__(self):
+ ## (C) - Flames ASI HALTMANN CO LABS 2022-2050
+
+## geenrate the gui and make it look NICE >:) 
+import os
+import sys
+import time
+import random
+import webbrowser
+import subprocess
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import simpledialog
+# generate a gui that looks cool and makes high quality rips
+# this is the default version of the program
+generatesound = tk.Tk()
+generatesound.title("HQRipster")
+generatesound.geometry("500x500")
+generatesound.resizable(0,0)
+generatesound.configure(background='#000000')
+generatemashup = generatesound.geometry("500x500")  # this is the default version of the program
+## this activates the vipping process
+## install wavenet GAN to python
+
+vip = wavenet.vip()
+## this activates the vipping process
+tk.Tk()
+vip.title("HQRipster")
+vip.geometry("500x500")
+vip.resizable(0,0)
+vip.configure(background='#000000')
+vip.geometry("500x500")
+## this activates the vipping process
+print(vip.configure(background='#000000'))
+## this is the default version of the program
